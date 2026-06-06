1b78c3db23f14592ea07774f47c84b0d2fec89e5
demirrana <demir.rana.bo@gmail.com>
2026-06-06 17:22:54 +0300

    enemies move toward baby now

--- Assets/Scripts/EnemyNavigator.cs
+++ Assets/Scripts/EnemyNavigator.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class EnemyAI : MonoBehaviour
+public class EnemyNavigator : MonoBehaviour
 {
     [Header("Target Settings")]
     [SerializeField] private Transform target;
 
     private NavMeshAgent agent;
 
+    private void OnEnable()
+    {
+        target = GameEvents.GetBabyTransform.Invoke();
+        Debug.Log("EnemyNavigator: Target set to " + target.name);
+    }
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        agent.speed = 3.5f;
+        agent.acceleration = 12f;
+        agent.stoppingDistance = 0.5f;
     }
 
     private void Update()
@@ -19,6 +32,7 @@ public class EnemyAI : MonoBehaviour
         if (target != null)
         {
             agent.SetDestination(target.position);
+            Debug.Log("EnemyNavigator: Moving towards target at " + target.position);
         }
     }
 }
